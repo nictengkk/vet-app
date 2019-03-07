f943a9a7d5d0af1cf885653758a10303fec74d5e
nictengkk <nictengkk@gmail.com>
2019-03-07 17:41:46 +0800

    updated componentDidMount method to render fetch api to update state

--- src/components/ClinicList/ClinicList.jsx
+++ src/components/ClinicList/ClinicList.jsx
@@ -2,6 +2,24 @@ import React, { Component } from "react";
 import Clinic from "../Clinic/Clinic";
 
 class ClinicList extends Component {
+  state = {
+    clinicList: [],
+    userAddress: ""
+  };
+
+  async componentDidMount() {
+    try {
+      const response = await fetch(
+        "https://data.gov.sg/api/action/datastore_search?resource_id=b2871270-4eef-44a3-be98-908e2a73b19f"
+      );
+      const data = await response.json();
+      this.setState({ clinicList: data.result.records });
+    } catch (error) {
+      console.log(error);
+    }
+
+    //
+  }
   // fetchPhotos = async searchTerm => {
   //   try {
   //     const response = await fetch(
@@ -25,7 +43,11 @@ class ClinicList extends Component {
       <div className="container">
         {clinicList.map(clinic => (
           <div className="card-col" key={clinic._id}>
-            <Clinic name={clinic.name} type={clinic.type} />
+            <Clinic
+              name={clinic.name}
+              type={clinic.type}
+              distance={clinic.distance}
+            />
           </div>
         ))}
       </div>
