a413477fb171abcdc2362d11a96b013986dedde5
nictengkk <nictengkk@gmail.com>
2019-03-08 20:12:24 +0800

    Create clinic table component to store clinics instead of using cards because of lack of image

---
+++ src/components/ClinicTable/ClinicTable.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+function ClinicTable({ clinicList }) {
+  //   const sortedList = clinicsList.sort((first, second) => {
+  //     if (first.name < second.name) return -1;
+  //     if (first.name > second.name) return 1;
+  //     return 0;
+  //   });
+
+  //   const number = () => {};
+
+  const styles = {
+    height: window.innerHeight,
+    width: 550
+  };
+
+  return (
+    <table className="table" style={styles}>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Address</th>
+          <th>Type</th>
+          <th>Postal Code</th>
+          <th>Contact Number</th>
+        </tr>
+      </thead>
+      <tbody>
+        {clinicList.map(clinic => (
+          <tr key={clinic._id} data-testid="clinic-table-row">
+            <td>{clinic.name}</td>
+            <td>{clinic.address}</td>
+            <td>{clinic.type}</td>
+            <td>{clinic.postal_code}</td>
+            <td>{clinic.tel_office_1}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default ClinicTable;
