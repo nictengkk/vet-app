3879a4dc1a80750940fd67f883e6394987f729d2
nictengkk <nictengkk@gmail.com>
2019-05-07 19:06:02 +0800

    add handleClick to handle more info button

--- src/views/ClinicInfoPage/ClinicInfoPage.jsx
+++ src/views/ClinicInfoPage/ClinicInfoPage.jsx
@@ -3,6 +3,11 @@ import ClinicDrawer from "../../components/ClinicDrawer/ClinicDrawer";
 import ClinicDetailed from "../../components/ClinicDetailed/ClinicDetailed";
 import SimpleMap from "../../components/MapGL/SimpleMap";
 
+const isDev = process.env.NODE_ENV !== "production";
+const getUrl = isDev
+  ? "http://localhost:5555"
+  : "https://backend-for-vet-app.herokuapp.com";
+
 export class ClinicInfoPage extends Component {
   state = {
     clinic: {
@@ -11,12 +16,43 @@ export class ClinicInfoPage extends Component {
       tel_office: "",
       address: "",
       postalCode: "",
-      reviews: []
+      reviews: [],
+      coordinate: {}
     }
   };
 
   styles = {
     width: "500px"
+  };
+
+  handleClick = async id => {
+    try {
+      const response = await fetch(`${getUrl}/api/clinics/${id}`, {
+        credentials: "include"
+      });
+      const data = await response.json();
+      console.log(data);
+      const clinicID = data[0].id;
+      const clinicName = data[0].name;
+      const clinicAdd = data[0].address;
+      const clinicPC = data[0].postal_code;
+      const clinicTel = data[0].tel_office;
+      const clinicReviews = data[0].reviews;
+      const clinicCoordinate = data[0].coordinate;
+      this.setState({
+        clinic: {
+          name: clinicName,
+          address: clinicAdd,
+          postalCode: clinicPC,
+          tel_office: clinicTel,
+          id: clinicID,
+          reviews: clinicReviews,
+          coordinate: clinicCoordinate
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   async componentDidMount() {
@@ -47,9 +83,13 @@ export class ClinicInfoPage extends Component {
     const { combinedClinicList } = this.props;
     const { mapInitialCenter: userAddress } = this.props;
     const { clinic } = this.state;
+    console.log(clinic);
     return (
       <div>
-        <ClinicDrawer data={combinedClinicList} />
+        <ClinicDrawer
+          data={combinedClinicList}
+          handleClick={this.handleClick}
+        />
         <div className="container">
           <div className="row">
             <div className="col-lg-6">
@@ -63,6 +103,7 @@ export class ClinicInfoPage extends Component {
               <SimpleMap
                 userAddress={userAddress}
                 clinics={combinedClinicList}
+                clinic={clinic}
               />
             </div>
           </div>
