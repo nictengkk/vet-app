9a7f2c133645bd87ae274024bf1821f57a8c4a6d
nictengkk <nictengkk@gmail.com>
2019-05-07 19:02:15 +0800

    update Clinic to use reactstrap components

--- src/components/Clinic/Clinic.jsx
+++ src/components/Clinic/Clinic.jsx
@@ -1,17 +1,13 @@
 import React from "react";
-// import { Link } from "react-router-dom";
 import { Card, CardBody, CardTitle, CardFooter, Button } from "reactstrap";
 
 function Clinic(props) {
-  // const styles = { width: "18rem" };
-
   const { name, id, style, handleClick } = props;
   return (
     <div style={style}>
       <Card>
         <CardBody>
           <CardTitle>{name}</CardTitle>
-          {/* <CardSubtitle>{type}</CardSubtitle> */}
         </CardBody>
         <CardFooter>
           <Button
@@ -28,24 +24,3 @@ function Clinic(props) {
 }
 
 export default Clinic;
-
-/* <Link className="btn btn-primary btn-sm mb-2" to="/restaurants/new">
-  Create New
-</Link>; */
-
-// {/* <img src={images} className="card-img-top img-fluid" alt="clinic" /> */ }
-// <div className="card-body">
-//   <h5 className="card-title" data-testid="restaurant-card-title">
-//     {name}
-//   </h5>
-//   <h6 className="card-subtitle mb-2 text-muted">{type}</h6>
-// </div>
-
-//   <div className="card-footer text-muted">
-//     <Link
-//       className="btn btn-outline-primary btn-sm mx-1"
-//       to={`/clinics/${10}`}
-//     >
-//       More Info
-//         </Link>
-//   </div>
