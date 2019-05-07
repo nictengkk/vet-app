5b06b0220d7d424a7cc28cc2c2a72c9dac3e22f6
nictengkk <nictengkk@gmail.com>
2019-05-07 16:04:14 +0800

    update Clinic to use reactstrap components

--- src/components/Clinic/Clinic.jsx
+++ src/components/Clinic/Clinic.jsx
@@ -1,28 +1,28 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import { Card, CardBody, CardTitle, CardFooter, Button } from "reactstrap";
 
 function Clinic(props) {
   // const styles = { width: "18rem" };
 
-  const { name, type, style, data } = props;
+  const { name, id, style, handleClick } = props;
   return (
-    <div className="card my-3" style={style}>
-      {/* <img src={images} className="card-img-top img-fluid" alt="clinic" /> */}
-      <div className="card-body">
-        <h5 className="card-title" data-testid="restaurant-card-title">
-          {name}
-        </h5>
-        <h6 className="card-subtitle mb-2 text-muted">{type}</h6>
-      </div>
-
-      <div className="card-footer text-muted">
-        <Link
-          className="btn btn-outline-primary btn-sm mx-1"
-          to={`/clinics/${10}`}
-        >
-          More Info
-        </Link>
-      </div>
+    <div style={style}>
+      <Card>
+        <CardBody>
+          <CardTitle>{name}</CardTitle>
+          {/* <CardSubtitle>{type}</CardSubtitle> */}
+        </CardBody>
+        <CardFooter>
+          <Button
+            onClick={() => {
+              handleClick(id);
+            }}
+          >
+            More Info
+          </Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
@@ -32,3 +32,20 @@ export default Clinic;
 /* <Link className="btn btn-primary btn-sm mb-2" to="/restaurants/new">
   Create New
 </Link>; */
+
+// {/* <img src={images} className="card-img-top img-fluid" alt="clinic" /> */ }
+// <div className="card-body">
+//   <h5 className="card-title" data-testid="restaurant-card-title">
+//     {name}
+//   </h5>
+//   <h6 className="card-subtitle mb-2 text-muted">{type}</h6>
+// </div>
+
+//   <div className="card-footer text-muted">
+//     <Link
+//       className="btn btn-outline-primary btn-sm mx-1"
+//       to={`/clinics/${10}`}
+//     >
+//       More Info
+//         </Link>
+//   </div>
